51627db4688cf6f35399aa5c5916febedd43b328
Merna Bassem <mernabassem21@gmail.com>
2024-08-01 12:50:43 +0300

    cinnection in db

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { config } from "dotenv";
+import {connection_db} from "./DB/connection.js";
+
+config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+
+connection_db();
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
