869569ffacc4f4b7e0413b3eca706e44473957c6
Merna Bassem <mernabassem21@gmail.com>
2024-08-03 14:43:16 +0300

    resolve validation in get category

--- src/Modules/Categories/categories.schema.js
+++ src/Modules/Categories/categories.schema.js
@@ -24,23 +24,20 @@ export const createCategorySchema = {
 
 export const getCategorySchema = {
   query: Joi.object({
-    name:Joi.string().messages({
-      "string.base":"Category Name must be a string"
+    name: Joi.string().messages({
+      "string.base": "Category Name must be a string"
     }),
     id: Joi.string()
       .custom(objectIdValidation, "Object ID Validation")
-      .required()
       .messages({
-        "any.required": "Category ID is required",
         "string.base": "Category ID must be a string",
         "string.pattern": "Category ID must be a valid ObjectId",
       }),
-      slug:Joi.string().messages({
-      "string.base":"Category Name must be a string"
+    slug: Joi.string().messages({
+      "string.base": "Category Slug must be a string"
     }),
   }).min(1)
     .messages({
-      "object.min":
-        "At least one field is required send in query parameters sush as workingTime,jobLocation,seniorityLevel,jobTitle,technicalSkills,softSkills",
+      "object.min": "At least one field is required in query parameters such as name, id, or slug",
     }),
 }
