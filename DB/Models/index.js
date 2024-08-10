6a92048ee5a2d305a1c3f26fd49d1dbaa6768e4e
Merna Bassem <mernabassem21@gmail.com>
2024-08-10 14:28:19 +0300

    product model

--- DB/Models/index.js
+++ DB/Models/index.js
@@ -1,3 +1,4 @@
 export * from "./category.models.js"
 export * from "./sub-category.models.js"
 export * from "./brand.models.js"
+export * from "./product.models.js"
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,5 @@ export * from "./error-class.utils.js";
 export * from "./cloudinary.utils.js";
 export * from "./system-roles.utils.js";
 export * from "./general-rules.utils.js";
+export * from "./enums.utils.js";
+export * from "./calculate-price.utils.js";
