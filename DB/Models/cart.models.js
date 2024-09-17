13cbaf9129c2bc109b37c68d796b507d8b889a6e
Merna Bassem <mernabassem21@gmail.com>
2024-09-17 10:58:59 +0300

    cart models

---
+++ DB/Models/cart.models.js
@@ -0,0 +1,41 @@
+import { required } from "joi";
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const cartSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    products: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 1,
+          min: 1,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    subTotal: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Cart = mongoose.models.Cart || model("Cart", cartSchema);
